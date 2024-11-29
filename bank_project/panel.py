66ae4f7a85a4f10d51972e9692c7758b6bb32285
Cricko7 <161488478+Cricko7@users.noreply.github.com>
2024-11-29 21:32:05 +0300

    panel.py2

--- bank_project/panel.py
+++ bank_project/panel.py
@@ -1,11 +1,30 @@
 #panel
-from flask import Flask
+import os
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Создаем папку для загрузок, если она не существует
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('bank-intrface.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    files = request.files.getlist('files')
+    
+    for file in files:
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    return 'ok'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
